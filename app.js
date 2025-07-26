6d93771e9ea899efe26a19cd488e8ea54fa63655
Piyus Khatri <khatripiyus95@gmail.com>
2025-07-27 00:30:42 +0545

    works fine maybe

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Global variables
         let tutors = [];
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = '/api';
         
         // Device fingerprinting function
         function generateDeviceFingerprint() {
